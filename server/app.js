5277858f74cf74abd7130b65554620d4e873e4b4
psharneja <psharneja@gmail.com>
2017-06-12 19:17:29 +0530

    get/post communities request on random table, to be changed to correct datamodel

--- server/app.js
+++ server/app.js
@@ -3,6 +3,13 @@ const path = require('path');
 
 app = express();
 
+const community = require('./modules/community/index');
+const members = require('./modules/members/index');
+
+
+
+
+
 let clientPath = path.resolve(__dirname, '..', 'dist');
 console.log('Hosting static path ', clientPath);
 app.use(express.static(clientPath));
@@ -15,8 +22,15 @@ app.get('/welcome', (req, res) => {
   res.send({msg: 'Welcome to app'});
 })
 
+
+
+app.use('/api/community', community );
+
 app.use((req, res) => {
   res.status(404).send({error: 'Resource not found'});
 })
+
+
+
 
 module.exports = app;
---
+++ server/connect.js
@@ -0,0 +1,8 @@
+const connectionString = {
+	"keyspace": "facebook",
+	"contact": "127.0.0.1",
+	"port": "9042",
+}
+
+
+module.exports = connectionString;
