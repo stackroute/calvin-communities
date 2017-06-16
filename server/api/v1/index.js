7ea7651995f895cec2ebb6d4e8d3b1d8c281ca09
JananiVenkat <jananivenkat94@gmail.com>
2017-06-16 18:34:10 +0530

    Indentation done

--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -4,14 +4,14 @@ router.use('/community', require('../../modules/community'));
 
 router.use('/invitation', require('../../modules/memberrequests'));
 
-router.use('/tools', 			require('../../modules/tools/'));
+router.use('/tools', require('../../modules/tools/'));
 
-router.use('/counter', 		require('../../modules/communitiescounter/'));
+router.use('/counter', require('../../modules/communitiescounter/'));
 
-router.use('/', 					require('../../modules/member/'));
+router.use('/', require('../../modules/member/'));
 
 router.use('/communityrole', require('../../modules/communityrole/'));
 
-router.use('/community', require('../../modules/communitytemplates/'));
+router.use('/communitytemplates', require('../../modules/communitytemplates/'));
 
 module.exports = router;
