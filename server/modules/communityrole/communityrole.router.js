711d6cab781e233c038808277302a41a611fae2a
prakharsrivastava25 <prakharsrivastava25@gmail.com>
2017-06-16 09:13:12 +0530

    removed unnecessary comments in role and service

--- server/modules/communityrole/communityrole.router.js
+++ server/modules/communityrole/communityrole.router.js
@@ -1,28 +1,8 @@
 const express = require('express');
-// const app = express();
-// const bodyParser = require('body-parser');
-// const service = require('./communityrole.service');
-// const model = require('cassandra-driver');
+
 const controller = require('./communityrole.controller');
-//const connectionString = require('../../connect');
-//var client = new model.Client({ 
-									// contactPoints: [connectionString.contact],
-									// 				protocolOptions: { port: connectionString.port },
-									// 				keyspace: connectionString.keyspace,
-								 // });
-
-
 
 const router = express.Router();
-
-/*router.get('/', (req, res) =>{
-	try{
-		res.send(controller.getcommunityrole(req, res));
-	}
-	catch (err){
-		res.send({error: 'Unexpected error from get method....please try later'});
-	}
-});*/
 
 router.get('/',controller.getcommunityrole);
 
