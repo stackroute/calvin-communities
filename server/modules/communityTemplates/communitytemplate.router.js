8a58723ace5c10ea4cbfd17fabae1ab6e429cdfe
JananiVenkat <jananivenkat94@gmail.com>
2017-06-16 14:14:54 +0530

    returned the value in the router

--- server/modules/communityTemplates/communitytemplate.router.js
+++ server/modules/communityTemplates/communitytemplate.router.js
@@ -4,18 +4,18 @@ const controller = require('./communitytemplate.controller');
 // route to display the list of templates
 router.get('/templates', (req, res) => {
   try {
-    res.status(200).json(controller.getListOfTemplates());
+     return res.send(controller.getListOfTemplates());
   } catch (err) {
-    res.status(404).json({ error: 'Unexpected internal error occurred, please try later...!' });
+    res.status(500)({ error: 'Unexpected internal error occurred, please try later...!' });
   }
 });
 
 // route to display the specified template data
 router.get('/templates/:templatename', (req, res) => {
   try {
-    res.status(200).json(controller.getSpecifiedTemplateData(req.params.templatename));
+    return res.send((controller.getSpecifiedTemplateData(req.params.templatename));
   } catch (err) {
-    res.status(404).json({ error: 'Unexpected internal error occurred, please try later...!' });
+    res.status(500)({ error: 'Unexpected internal error occurred, please try later...!' });
   }
 });
 
