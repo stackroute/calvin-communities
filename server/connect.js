f575ad6df4b56c3e54f0b10b677560d2b53b991e
SandhiyaViswanathan <sandhiya3795@gmail.com>
2017-06-16 14:08:44 +0530

    lint errors cleared

--- server/connect.js
+++ server/connect.js
@@ -1,17 +1,12 @@
 const connectionString = {
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 9358221781fb78383b75483ed3ccb881f2fc9b39
   keyspace: 'calvincommunity',
   contact: '127.0.0.1',
   port: '9042',
 };
-<<<<<<< HEAD
-=======
 
->>>>>>> 9358221781fb78383b75483ed3ccb881f2fc9b39
+
 
 
 module.exports = connectionString;
