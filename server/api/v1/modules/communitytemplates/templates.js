11fad0e4cbbc1f50e0cf12d2ecb48ba6fc4f9c14
JananiVenkat <jananivenkat94@gmail.com>
2017-06-18 13:15:55 +0530

    Indentation done in template.js

--- server/api/v1/modules/communitytemplates/templates.js
+++ server/api/v1/modules/communitytemplates/templates.js
@@ -23,71 +23,71 @@ module.exports = [{
     ]
   }]
 }, {
-      purpose: 'technical',
-      description: 'This template will provides you the required tools and roles to create a Developer community',
-      tools: [{
-        toolId: 'Stackoverflow',
-        actions: ['postmesage', 'read', 'Likemessage', 'edit', 'share'],
-      }, {
-        toolId: 'Quora',
-        actions: ['postmesage', 'read', 'Likemessage', 'comment', 'share', 'invitation'],
-      }, {
-        toolId: 'Github',
-        actions: ['postmesage', 'read', 'Likemessage', 'edit', 'upload'],
-      }],
-      role_actions: [{
-        role: 'admin',
-        actions: [{ action: 'post', grant: 'self' },
-          { action: 'read', grant: 'self' },
-          { action: 'delete_post', grant: 'self' }
-        ]
-      }, {
-        role: 'member',
-        actions: [{ action: 'like', grant: 'self' },
-          { action: 'post', grant: 'self' }
-        ]
-      }]
-    }, {
-      purpose: 'Teaching',
-      description: 'This template will provides you the required tools and roles to create a teachers community',
-      tools: [{
-        toolId: 'theteacherscorner.net',
-        actions: ['postmesage', 'read', 'Likemessage'],
-      }, {
-        toolId: 'scholastic.com',
-        actions: ['postmesage', 'read', 'Likemessage', 'edit'],
-      }],
-        role_actions: [{
-          role: 'member',
-          actions: [{ action: 'post', grant: 'self' },
-            { action: 'read', grant: 'self' }
-          ]
-        }, {
-          role: 'moderator',
-          actions: [{ action: 'edit', grant: 'self' },
-            { action: 'post', grant: 'self' }
-          ]
-        }]
-      }, {
-        purpose: 'Arts',
-        description: 'This template will provides you the required tools and roles to create a artist\'s community',
-        tools: [{
-          toolId: 'theabundantartist.com',
-          actions: ['postmesage', 'read', 'Likemessage'],
-        }, {
-          toolId: 'www.creativebloq.com',
-          actions: ['postmesage', 'read', 'Likemessage', 'edit', 'share', 'upload'],
-        }],
-        role_actions: [{
-          role: 'admin',
-          actions: [{ action: 'post', grant: 'post_self' },
-            { action: 'read', grant: 'self' }
-          ]
-        }, {
-          role: 'editor',
-          actions: [{ action: 'edit', grant: 'self' },
-            { action: 'post', grant: 'self' },
-            { action: 'read', grant: 'self' }
-           ]
-      }]
-    }];
+  purpose: 'technical',
+  description: 'This template will provides you the required tools and roles to create a Developer community',
+  tools: [{
+    toolId: 'Stackoverflow',
+    actions: ['postmesage', 'read', 'Likemessage', 'edit', 'share'],
+  }, {
+    toolId: 'Quora',
+    actions: ['postmesage', 'read', 'Likemessage', 'comment', 'share', 'invitation'],
+  }, {
+    toolId: 'Github',
+    actions: ['postmesage', 'read', 'Likemessage', 'edit', 'upload'],
+  }],
+  role_actions: [{
+    role: 'admin',
+    actions: [{ action: 'post', grant: 'self' },
+      { action: 'read', grant: 'self' },
+      { action: 'delete_post', grant: 'self' }
+    ]
+  }, {
+    role: 'member',
+    actions: [{ action: 'like', grant: 'self' },
+      { action: 'post', grant: 'self' }
+    ]
+  }]
+}, {
+  purpose: 'Teaching',
+  description: 'This template will provides you the required tools and roles to create a teachers community',
+  tools: [{
+    toolId: 'theteacherscorner.net',
+    actions: ['postmesage', 'read', 'Likemessage'],
+  }, {
+    toolId: 'scholastic.com',
+    actions: ['postmesage', 'read', 'Likemessage', 'edit'],
+  }],
+  role_actions: [{
+    role: 'member',
+    actions: [{ action: 'post', grant: 'self' },
+      { action: 'read', grant: 'self' }
+    ]
+  }, {
+    role: 'moderator',
+    actions: [{ action: 'edit', grant: 'self' },
+      { action: 'post', grant: 'self' }
+    ]
+  }]
+}, {
+  purpose: 'Arts',
+  description: 'This template will provides you the required tools and roles to create a artist\'s community',
+  tools: [{
+    toolId: 'theabundantartist.com',
+    actions: ['postmesage', 'read', 'Likemessage'],
+  }, {
+    toolId: 'www.creativebloq.com',
+    actions: ['postmesage', 'read', 'Likemessage', 'edit', 'share', 'upload'],
+  }],
+  role_actions: [{
+    role: 'admin',
+    actions: [{ action: 'post', grant: 'post_self' },
+      { action: 'read', grant: 'self' }
+    ]
+  }, {
+    role: 'editor',
+    actions: [{ action: 'edit', grant: 'self' },
+      { action: 'post', grant: 'self' },
+      { action: 'read', grant: 'self' }
+    ]
+  }]
+}];
