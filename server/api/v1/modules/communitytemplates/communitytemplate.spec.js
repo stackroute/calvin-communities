07a996dc665b5c75062dccec73435b4eff3c14e4
JananiVenkat <jananivenkat94@gmail.com>
2017-06-18 12:57:06 +0530

    removed templatename, added tool actions and role actions in the template.js

--- server/api/v1/modules/communitytemplates/communitytemplate.spec.js
+++ server/api/v1/modules/communitytemplates/communitytemplate.spec.js
@@ -7,92 +7,102 @@ const templates = require('./templates');
 
 
 const surgeon = {
-    templateName: 'surgeon',
-    purpose: 'Medical',
-    description: 'This template will provides you the required tools and roles to create a surgeon community',
-    tools: [{
-        Id: 100,
-        Name: 'Digital Healthcare',
-        Description: 'connecting verified and credentialed physicians from countries around the world.',
-    }, {
-        Id: 200,
-        Name: 'WeMedUp',
-        Description: 'connecting verified and credentialed physicians from countries around the world.',
-    }, {
-        Id: 500,
-        Name: 'sermo',
-        Description: 'connecting verified and credentialed physicians from countries around the world.',
-    }],
-    roles: ['Admin', 'Moderator', 'Member'],
+  templateName: 'surgeon',
+  purpose: 'Medical',
+  description: 'This template will provides you the required tools and roles to create a surgeon community',
+  tools: [{
+    Id: 100,
+    Name: 'Digital Healthcare',
+    Description: 'connecting verified and credentialed physicians from countries around the world.',
+  }, {
+    Id: 200,
+    Name: 'WeMedUp',
+    Description: 'connecting verified and credentialed physicians from countries around the world.',
+  }, {
+    Id: 500,
+    Name: 'sermo',
+    Description: 'connecting verified and credentialed physicians from countries around the world.',
+  }],
+  roles: ['Admin', 'Moderator', 'Member'],
 };
 
 describe('Retrieve the specified template data', () => {
-    it(' should retrieve specified template data ', (done) => {
-        request(app)
+  it(' should retrieve specified template data ', (done) => {
+    request(app)
             .get('/community/templates/surgeon')
             .end((err, res) => {
-                if (err) { done(err);
-                    return; }
-                templates.filter((element) => {
-                    if (element.templateName == surgeon)
+              if (err) {
+                done(err);
+                return;
+              }
+              templates.filter((element) => {
+                if (element.templateName == surgeon)
                     // expect(res.body).to.deep.equal(element.templateName);
                     { res.body.should.deep.equal(element); }
-                });
-                done();
+              });
+              done();
             });
-    });
-    describe('Specified template does not exist', () => {
-        it('the specified template does not exist', (done) => {
-            request(app)
+  });
+  describe('Specified template does not exist', () => {
+    it('the specified template does not exist', (done) => {
+      request(app)
                 .get('/community/templates/travel')
                 .end((err, res) => {
-                    if (err) { done(err);
-                        return; }
-                    res.status.should.be.equal(404);
-                    done();
+                  if (err) {
+                    done(err);
+                    return;
+                  }
+                  res.status.should.be.equal(404);
+                  done();
                 });
-        });
     });
-    describe('List the templates ', () => {
-        it('list of templates are equal', (done) => {
-            request(app)
+  });
+  describe('List the templates ', () => {
+    it('list of templates are equal', (done) => {
+      request(app)
                 .get('/community/templates')
                 .end((err, res) => {
-                    if (err) { done(err);
-                        return; }
-                    res.body.should.deep.equal(templates);
-                    res.status.should.equal(200);
-                    done();
+                  if (err) {
+                    done(err);
+                    return;
+                  }
+                  res.body.should.deep.equal(templates);
+                  res.status.should.equal(200);
+                  done();
                 });
-        });
     });
-    describe('List the template tools', () => {
-        it('Display the tools for the templates', (done) => {
-            request(app)
+  });
+  describe('List the template tools', () => {
+    it('Display the tools for the templates', (done) => {
+      request(app)
                 .get('/community/templates')
                 .end((err, res) => {
-                    if (err) { done(err);
-                        return; }
-                    templates.filter((element) => {
+                  if (err) {
+                    done(err);
+                    return;
+                  }
+                  templates.filter((element) => {
                         // let tools = element.tools;
                         // expect(tools).should.be.equal()
-                        res.status.should.be.equal(200);
-                    });
-                    done();
+                    res.status.should.be.equal(200);
+                  });
+                  done();
                 });
-        });
     });
-    describe('Roles should be an array', () => {
-        it('roles should be array', (done) => {
-            request(app)
+  });
+  describe('Roles should be an array', () => {
+    it('roles should be array', (done) => {
+      request(app)
                 .get('/community/templates')
                 .end((err, res) => {
-                    if (err) { done(err);
-                        return; }
-                    templates.filter((element) => {
-                        res.status.should.be.equal(200);
-                    });
+                  if (err) {
+                    done(err);
+                    return;
+                  }
+                  templates.filter((element) => {
+                    res.status.should.be.equal(200);
+                  });
                 });
-        });
     });
+  });
 });
