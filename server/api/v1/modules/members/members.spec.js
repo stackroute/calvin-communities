9cac4b2c0412a348ce05cf8040affc518a8bcb5c
Aravindh-r <aravindhchandran94@gmail.com>
2017-06-19 15:56:33 +0530

    test cases for all service

--- server/api/v1/modules/members/members.spec.js
+++ server/api/v1/modules/members/members.spec.js
@@ -14,31 +14,31 @@ const client = new model.Client({
   keyspace: connectionString.keyspace,
 });
 
-const COMMUNITY_MEMBERSHIP_TABLE = 'communitymembership';
+const MEMBERS_TABLE = 'members';
 
-describe('Create a community and update it', () => {
+describe('Members community details and update it', () => {
   before(() => {
         // runs before all tests in this block
 
-    client.execute(`DELETE FROM ${COMMUNITY_MEMBERSHIP_TABLE} where domain='Stack-Route-Immersive'`);
+    client.execute(`DELETE FROM ${MEMBERS_TABLE} where username='Aravindh'`);
   });
 
   it('Add new member to a community', (done) => {
     request(app)
-            .post('/api/v1/membership/community/member/role')
+            .post('/api/v1/members/member/community/role')
             .send({
               domain: 'Stack-Route-Immersive',
               username: 'Aravindh',
               role: 'Trainee-FullStack-Developer',
             })
             .then(() => {
-              client.execute(`SELECT * FROM ${COMMUNITY_MEMBERSHIP_TABLE} where domain='Stack-Route-Immersive'`, (err, result) => {
+              client.execute(`SELECT * FROM ${MEMBERS_TABLE} where username='Aravindh' AND domain='Stack-Route-Immersive'`, (err, result) => {
                 console.log(result.rows[0]);
                 result.rows.length.should.be.equal(1);
                 result.rows[0].domain.should.be.equal('Stack-Route-Immersive');
                 result.rows[0].username.should.be.equal('Aravindh');
                 result.rows[0].role.should.be.equal('Trainee-FullStack-Developer');
-                console.log('Data added...');
+                console.log('Data Added...');
               });
               done();
             })
@@ -49,22 +49,22 @@ describe('Create a community and update it', () => {
 
   it('Update a community member', (done) => {
     request(app)
-            .patch('/api/v1/membership/member/Aravindh/community/Stack-Route-Immersive/role')
+            .patch('/api/v1/members/community/Stack-Route-Immersive/role/member/Aravindh')
             .send({
               role: 'Trainee-FullStack',
             })
             .then(() => {
-              client.execute(`SELECT * FROM ${COMMUNITY_MEMBERSHIP_TABLE} where domain='Stack-Route-Immersive' AND username='Aravindh' `, (err, result) => {
-                // console.log(result.rows);
+              client.execute(`SELECT * FROM ${MEMBERS_TABLE} where domain='Stack-Route-Immersive' AND username='Aravindh' `, (err, result) => {
                 if (!err) {
                   result.rows.length.should.be.equal(1);
                   result.rows[0].domain.should.be.equal('Stack-Route-Immersive');
                   result.rows[0].username.should.be.equal('Aravindh');
                   result.rows[0].role.should.be.equal('Trainee-FullStack');
-                  console.log(result.rows);
-                }
-                if (err) {
-                  console.log('This community not have a member of this name...!!');
+                  console.log(result.rows[0]);
+                  console.log('Updated');
+                } if (err) {
+                  console.log(result.rows[0]);
+                  console.log('This member is not a part of a community');
                 }
               });
               done();
@@ -76,9 +76,9 @@ describe('Create a community and update it', () => {
 
   it('Get a member of community with role', (done) => {
     request(app)
-      .get('/api/v1/membership/community/Stack-Route-Immersive/members')
+      .get('/api/v1/members/member/Aravindh/communities')
       .then(() => {
-        client.execute(`SELECT * FROM ${COMMUNITY_MEMBERSHIP_TABLE} where domain='Stack-Route-Immersive' `, (err, result) => {
+        client.execute(`SELECT * FROM ${MEMBERS_TABLE} where username='Aravindh' `, (err, result) => {
           if (!err) {
             result.rows.length.should.be.equal(1);
             console.log(result.rows);
@@ -91,13 +91,14 @@ describe('Create a community and update it', () => {
           done(err);
         });
   });
+
   it('Delete a community member', (done) => {
     request(app)
-            .delete('/api/v1/membership/community/Stack-Route-Immersive/removemember/Aravindh')
+            .delete('/api/v1/members/removemember/Aravindh/community/Stack-Route-Immersive')
             .then(() => {
-              client.execute(`SELECT * FROM ${COMMUNITY_MEMBERSHIP_TABLE} where domain='Stack-Route-Immersive' AND username='Aravindh' `, (err) => {
+              client.execute(`SELECT * FROM ${MEMBERS_TABLE} where domain='Stack-Route-Immersive' AND username='Aravindh' `, (err) => {
                 if (!err) {
-                  console.log('Data Deleted..');
+                  console.log('Data deleted....');
                 }
                 if (err) {
                   console.log('This community not have a member of this name...!!');
@@ -109,22 +110,23 @@ describe('Create a community and update it', () => {
               done(err);
             });
   });
-  it('Add new member to a community', (done) => {
+
+  it('Again adding new member to a community', (done) => {
     request(app)
-            .post('/api/v1/membership/community/member/role')
+            .post('/api/v1/members/member/community/role')
             .send({
               domain: 'Stack-Route-Immersive',
               username: 'Aravindh',
               role: 'Trainee-FullStack-Developer',
             })
             .then(() => {
-              client.execute(`SELECT * FROM ${COMMUNITY_MEMBERSHIP_TABLE} where domain='Stack-Route-Immersive'`, (err, result) => {
+              client.execute(`SELECT * FROM ${MEMBERS_TABLE} where username='Aravindh' AND domain='Stack-Route-Immersive'`, (err, result) => {
                 console.log(result.rows[0]);
                 result.rows.length.should.be.equal(1);
                 result.rows[0].domain.should.be.equal('Stack-Route-Immersive');
                 result.rows[0].username.should.be.equal('Aravindh');
                 result.rows[0].role.should.be.equal('Trainee-FullStack-Developer');
-                console.log('Data added again...');
+                console.log('Again data added...');
               });
               done();
             })
@@ -133,8 +135,7 @@ describe('Create a community and update it', () => {
             });
   });
 
-
   after('', () => {
-    client.execute(`DELETE FROM ${COMMUNITY_MEMBERSHIP_TABLE} where domain='Stack-Route-Immersive'`);
+    client.execute(`DELETE FROM ${MEMBERS_TABLE} where username='Aravindh' and domain ='Stack-Route-Immersive'`);
   });
 });
