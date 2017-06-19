1ebc375621f8b3a3e21e532c3554e47e33e66e4d
Aravindh-r <aravindhchandran94@gmail.com>
2017-06-19 10:01:42 +0530

    test cases in service

--- server/api/v1/modules/communityMembership/communityMembership.spec.js
+++ server/api/v1/modules/communityMembership/communityMembership.spec.js
@@ -1,4 +1,83 @@
-/* const should = require('chai').should();
+const should = require('chai').should();
+
+const app = require('../../../../app');
+
+const request = require('supertest');
+
+const model = require('cassandra-driver');
+
+const connectionString = require('../../../../config');
+
+const client = new model.Client({
+  contactPoints: [connectionString.contact],
+  protocolOptions: { port: connectionString.port },
+  keyspace: connectionString.keyspace,
+});
+
+const COMMUNITY_MEMBERSHIP_TABLE = 'communitymembership';
+
+describe('Create a community and update it', () => {
+  before(() => {
+        // runs before all tests in this block
+
+    client.execute(`DELETE FROM ${COMMUNITY_MEMBERSHIP_TABLE} where domain='Stack-Route-Immersive'`);
+  });
+
+  it('Add new member to a community', (done) => {
+    request(app)
+            .post('/api/v1/membership/community/member/role')
+            .send({
+              domain: 'Stack-Route-Immersive',
+              username: 'Aravindh',
+              role: 'Trainee-FullStack-Developer',
+            })
+            .then(() => {
+              client.execute(`SELECT * FROM ${COMMUNITY_MEMBERSHIP_TABLE} where domain='Stack-Route-Immersive'`, (err, result) => {
+                console.log(result.rows[0]);
+                result.rows.length.should.be.equal(1);
+                result.rows[0].domain.should.be.equal('Stack-Route-Immersive');
+                result.rows[0].username.should.be.equal('Aravindh');
+                result.rows[0].role.should.be.equal('Trainee-FullStack-Developer');
+              });
+              done();
+            })
+            .catch((err) => {
+              done(err);
+            });
+  });
+
+  it('Update a community member', (done) => {
+    request(app)
+            .patch('/api/v1/membership/member/:username/community/:domain/role')
+            .send({
+              role: 'Trainee-FullStack',
+            })
+            .then(() => {
+              client.execute(`SELECT * FROM ${COMMUNITY_MEMBERSHIP_TABLE} where domain='Stack-Route-Immersive' AND username='Aravindh' IF EXISTS`, (err, result) => {
+                // console.log(result.rows);
+                result.rows.length.should.be.equal(1);
+                result.rows[0].domain.should.be.equal('Stack-Route-Immersive');
+                result.rows[0].username.should.be.equal('Aravindh');
+                // result.rows[0].role.should.be.equal('Trainee-FullStack');
+                console.log(result.rows);
+              });
+              done();
+            })
+            .catch((err) => {
+              done(err);
+            });
+  });
+  // after('',() =>{
+  //   client.execute(`DELETE FROM ${COMMUNITY_MEMBERSHIP_TABLE} where domain='Stack-Route-Immersive'`);
+  // });
+});
+
+/* after('', () => {
+  client.execute("DELETE FROM ${COMMUNITY_MEMBERSHIP_TABLE} where domain='Stack-Route-Immersive'");
+  });
+
+
+ const should = require('chai').should();
 
 const app = require('../../../../app');
 
@@ -61,7 +140,7 @@ describe('Add new member to community', function() {
     const query = (`DELETE FROM ${COMMUNITY_MEMBERSHIP_TABLE}`);
     client.execute(query);
   });
-});*/
+});
 
 const should = require('chai').should();
 
@@ -101,5 +180,4 @@ describe('Add new member to community', function() {
     });
     return done();
   });
-
-}
+}*/
