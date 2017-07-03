f94fe2beebb7f4c60b467606fd0f90b6166ab794
psharneja <psharneja@gmail.com>
2017-07-03 14:35:49 +0530

    lint errors removed for kafka consunmer

--- server/common/kafkaBaseConsumer/index.js
+++ server/common/kafkaBaseConsumer/index.js
@@ -1,26 +1,26 @@
-'use strict';
-
 const kafka = require('kafka-node');
+
+const logger = require('../../logger');
+
 const Consumer = kafka.Consumer;
 const Client = kafka.Client;
 const client = new Client('localhost:2181');
 const options = { autoCommit: true, fetchMaxWaitMs: 1000, fetchMaxBytes: 1024 * 1024 };
 
 function baseConsumer(topics, done) {
+  const consumer = new Consumer(client, topics, options);
 
+  consumer.on('message', (message) => {
+    done(null, message);
+    logger.debug('value is', message);
+  });
 
-    const consumer = new Consumer(client, topics, options);
+  consumer.on('error', (err) => {
+    done(err, null);
+    logger.debug('error', err);
+  });
+}
 
-    consumer.on('message', function(message) {
-        done(null, message);
-        console.log("value is", message);
-    });
-
-    consumer.on('error', function(err) {
-        done(err, null);
-        console.log('error', err);
-    });
-
+module.exports = {
+  baseConsumer,
 };
-
-
