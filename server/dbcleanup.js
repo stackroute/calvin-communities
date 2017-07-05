080ba3891ad0b7dc9458622c809df1abf91e7104
psharneja <psharneja@gmail.com>
2017-07-06 00:53:02 +0530

    changes made in package json to run all API testcases with npm run test

--- server/dbcleanup.js
+++ server/dbcleanup.js
@@ -100,7 +100,8 @@ client.connect()
 .then(() => {
   logger.debug(`table ${TABLE_COUNTER} cleared`);
   client.shutdown();
-  return logger.debug('all required tables cleared');
+  logger.debug('all required tables cleared');
+  process.exit();
 })
 .catch((err) => {
   client.shutdown();
