734238ae02230753a2bc370a089f403471292e5e
prakharsrivastava25 <prakharsrivastava25@gmail.com>
2017-07-07 17:48:36 +0530

    modified testcase

--- server/api/v1/modules/communityrole/communityrole.spec.js
+++ server/api/v1/modules/communityrole/communityrole.spec.js
@@ -43,108 +43,127 @@ const patchdata = [{
   },
   toolId: 'quora_patch',
 }];
+
 describe('Create a communityrole and update it', () => {
-  before(() => {});
+  before(() => {
+    client.execute("DELETE FROM communityroles where domain='prakhar'");
+    client.execute("DELETE FROM communityroles where domain='africans'");
+  });
+
   it('Create a new communityrole', (done) => {
     request(app)
       .post('/api/v1/communityrole/africans')
       .send(postdata)
-      .then(() => {
+      .end((err,res) => {
         logger.debug(`BEFORE CLIENT EXECUTE testcase${postdata}`);
         client.execute('SELECT * from communityroles where domain=\'africans\'', (err, result) => {
           if (!err) {
+            logger.debug("Result from post Api LOOK HERE",res.text);
+            logger.debug("result from client.execute",result);
             logger.debug(`Result from testcase ${result.rows[0]}`);
+            res.text.should.be.equal("Added");
             result.rows.length.should.be.equal(2);
             result.rows[0].actions.should.deep.equal(postdata[0].actions);
+            result.rows.find((elem) => {
+              elem.rolename == postdata[0]
+            })
+
+            result.rows.find((elem) => {
+              elem.rolename == postdata[1].rolename
+            });
             done();
           }
-        });
-      })
-      .catch((err) => {
-        logger.debug('In error');
+          else{
+            logger.debug('In error');
         done(err);
+          }
+        });
       });
   });
   it('Create a new communityrole', (done) => {
     request(app)
       .post('/api/v1/communityrole/prakhar')
       .send(postdata)
-      .then(() => {
+      .end((err,res) => {
+        logger.debug("Result from post Api LOOK HERE",res.text);
         logger.debug(`BEFORE CLIENT EXECUTE testcase${postdata}`);
         client.execute('SELECT * from communityroles where domain=\'prakhar\'', (err, result) => {
           if (!err) {
             logger.debug(`Result from testcase ${result.rows[0]}`);
+            res.text.should.be.equal("Added");
             result.rows.length.should.be.equal(2);
             result.rows[0].actions.should.deep.equal(postdata[0].actions);
             done();
           }
+          else{
+            done(err);
+          }
         });
       })
-      .catch((err) => {
-        logger.debug('In error');
-        done(err);
-      });
+
   });
 
   it('Create a new communityrole', (done) => {
     request(app)
       .get('/api/v1/communityrole/prakhar')
-      .then(() => {
+      .end((err,res) => {
+        logger.debug("Result from post Api LOOK HERE",res.text);
         client.execute('SELECT * from communityroles where domain=\'prakhar\'', (err, result) => {
           if (!err) {
+            logger.debug("Response is :",res.body);
             logger.debug(`Result from testcase ${result.rows[0]}`);
             result.rows.length.should.be.equal(2);
             done();
           }
+          else {
+            done(err);
+          }
         });
       })
-      .catch((err) => {
-        logger.debug('In error');
-        done(err);
-      });
   });
 
   it('Create a new communityrole', (done) => {
     request(app)
       .get('/api/v1/communityrole/africans')
-      .then(() => {
+      .end((err,res) => {
+        logger.debug("Result from post Api LOOK HERE",res.text);
         client.execute('SELECT * from communityroles where domain=\'africans\'', (err, result) => {
           if (!err) {
             logger.debug(`Result from testcase ${result.rows[0]}`);
             result.rows.length.should.be.equal(2);
             done();
           }
+          else{
+            done(err);
+          }
         });
       })
-      .catch((err) => {
-        logger.debug('In error');
-        done(err);
-      });
   });
 
 
   it('Create a new communityrole', (done) => {
     request(app)
       .get('/api/v1/communityrole/random')
-      .then(() => {
+      .end((err,res) => {
+        logger.debug("Result from post Api LOOK HERE",res.text);
         client.execute('SELECT * from communityroles where domain=\'random\'', (err, result) => {
           if (!err) {
             logger.debug(`Result from testcase ${result.rows[0]}`);
             result.rows.length.should.be.equal(0);
             done();
           }
+          else {
+            done(err);
+          }
         });
       })
-      .catch((err) => {
-        logger.debug('In error');
-        done(err);
-      });
   });
 
-  /*   it('Create a new communityrole', (done) => {
+/*          it('Test GET of distinct roles', (done) => {
       request(app)
-        .get('/api/v1/communityrole/smallu?onlyroles=true')
+        .get('/api/v1/communityrole/smallu?onlyroles=\'true\'')
         .then(() => {
+          
           client.execute('SELECT role from communityroles where domain=\'smallu\'',
            (err, result) => {
             if (!err) {
@@ -165,35 +184,37 @@ describe('Create a communityrole and update it', () => {
     request(app)
       .patch('/api/v1/communityrole/africans/roles/cricketer')
       .send(patchdata)
-      .then(() => {
+      .end((err,res) => {
+        logger.debug("Result from post Api LOOK HERE",res.text);
         client.execute("SELECT * from communityroles where domain='africans'", (err, result) => {
           if (!err) {
             result.rows[0].actions.should.have.property('post').equal(patchdata[0].actions.post);
             done();
           }
+          else{
+            done(err);
+          }
         });
       })
-      .catch((err) => {
-        done(err);
-      });
   });
   it('Update actions of communityrole', (done) => {
     request(app)
       .patch('/api/v1/communityrole/prakhar/roles/developer')
       .send(patchdata)
-      .then(() => {
+      .end((err,res) => {
+        logger.debug("Result from post Api LOOK HERE",res.text);
         client.execute("SELECT * from communityroles where domain='prakhar'", (err, result) => {
           if (!err) {
             result.rows[0].actions.should.have.property('post').equal(patchdata[0].actions.post);
             done();
           }
+          else{
+            done(err);
+          }
         });
       })
-      .catch((err) => {
-        done(err);
-      });
+     
   });
-
 
   after(() => {
     client.execute("DELETE FROM communityroles where domain='prakhar'");
