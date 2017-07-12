64c1a9fd733a608851cb97ffe42053b9598622b3
psharneja <psharneja@gmail.com>
2017-07-12 12:47:37 +0530

    added templates component

--- webapp/app/app-routing.module.ts
+++ webapp/app/app-routing.module.ts
@@ -33,7 +33,7 @@ const routes: Routes = [{
           },
           { path: '**', redirectTo: '/dashboard' }
 
- ], { useHash: true });
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ webapp/app/templates/templates.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {Http, Response} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class templatesService {
+
+		constructor(private http: Http) { };
+
+		getTemplates() {
+
+		}
+
+
+}
