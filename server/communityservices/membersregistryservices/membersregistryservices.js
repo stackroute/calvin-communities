3b0d41d500f5f6508fcdfcceab7510f34734f1f3
roshini <roshini>
2017-07-13 19:31:48 +0530

    created counter controller for event generation

--- server/communityservices/membersregistryservices/membersregistryservices.js
+++ server/communityservices/membersregistryservices/membersregistryservices.js
@@ -1,4 +1,5 @@
 const memberService = require('../../api/v1/modules/membership/membership.service');
+const countereventservice = require('../server/communityservices/communityCounterService')
 module.exports = function (eventMessage) {
   console.log('Got a new community event message: ', eventMessage);
   console.log('domain', eventMessage.domain);
@@ -9,4 +10,11 @@ module.exports = function (eventMessage) {
   	console.log('community list');
   }
   });
+  countereventservice.incrementmember(eventMessage.domain, (err, res)=>{
+  	if (err) {
+    console.log('error');
+  } else {
+  	console.log('memberincremented');
+  }
+});
 };
