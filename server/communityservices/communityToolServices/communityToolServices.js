31432836bb113031869a639bfb3e01b99f13808c
sandhyasrini <sandy150225@gmail.com>
2017-07-13 09:24:07 +0530

    test cases modified

--- server/communityservices/communityToolServices/communityToolServices.js
+++ server/communityservices/communityToolServices/communityToolServices.js
@@ -1,15 +1,15 @@
-const toolService = require('../../api/v1/modules/tools/tools.services');
+const toolService = require('../../api/v1/modules/tools/tools.controller');
 module.exports = function(eventMessage) {
   console.log('Got a new community event message: ', eventMessage);
   console.log(eventMessage.type);
   if (eventMessage.type === "add") {
-    toolService.addTools(eventMessage.tools, eventMessage.domain, (err, res) => {
+    toolService.postTools(eventMessage.tools, eventMessage.domain, (err, res) => {
       console.log('insde the tool');
     });
   }
   if (eventMessage.type === "delete") {
     console.log("delete", eventMessage);
-    toolService.deleteTools(eventMessage.domain, (err, res) => {
+    toolService.deleteTool(eventMessage.domain, (err, res) => {
       console.log('insde delete');
     });
   }
