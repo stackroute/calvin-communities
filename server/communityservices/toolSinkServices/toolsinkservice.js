d70403ba2f825c04c8ee0c0c85587df6e877823f
JananiVenkat <jananivenkat94@gmail.com>
2017-07-14 12:41:00 +0530

    added api to get all domains and tools from tools

--- server/communityservices/toolSinkServices/toolsinkservice.js
+++ server/communityservices/toolSinkServices/toolsinkservice.js
@@ -2,9 +2,21 @@ const toolSinkService = require('../../api/v1/modules/communitytools/communityto
 module.exports = function(eventMessage) {
   console.log('toolsink consumed the event: ', eventMessage);
   // console.log(eventMessage.type);
-  // if (eventMessage.type === "add") {
-  //   toolSinkService.checkTool(eventMessage.tools, eventMessage.domain, (err, res) => {
-  //     console.log('inside the tool');
-  //   });
-  // }
+    toolSinkService.checkTool(eventMessage.tools, eventMessage.domain, (err, res) => {
+      console.log('Registered tool successfully');
+      if(!err) {
+      toolSinkService.getTools(eventMessage.tools, eventMessage.domain), (error, result) => {
+      	if(!error) {
+      		console.log(result);
+      	} else {
+      		console.log(error);
+      	}
+      }
+    } else {
+    	console.log(err);
+    }
+    });
+
 };
+
+
