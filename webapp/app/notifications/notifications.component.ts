7a282c6a724a0cc6036ef5f8ffcb9fa4b64834cf
psharneja <psharneja@gmail.com>
2017-07-14 12:08:38 +0530

    changes in theme of calvin and sub-components added for dashboard

---
+++ webapp/app/notifications/notifications.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+
+@Component({
+	selector: 'calvin-notifications',
+  templateUrl: './notifications.component.html',
+  styleUrls: ['./notifications.component.css']
+})
+export class NotificationsComponent {
+
+}
