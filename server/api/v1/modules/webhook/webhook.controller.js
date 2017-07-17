224156fc3c304963f78f8e8eb4c74e9013d9891a
JananiVenkat <jananivenkat94@gmail.com>
2017-07-17 20:57:37 +0530

    webhook api added and verified the token

---
+++ server/api/v1/modules/webhook/webhook.controller.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const config = require('../../../config');
+const publishEvent = require('../../../../common/kafkaPublisher/kafkaPublisher');
+
+
+function verifyToken(token, done) {
+  console.log("verify token");
+  jwt.verify(token, config.appConstants.secret, (err, decoded) => {
+    if (err) {
+      console.log("error");
+      return done(err, "Unauthorized");
+    } else {
+      console.log("verified");
+      publishEvent.publishToTopic(topic, payload, (error, result) => {
+        if (error) {
+          return done(undefined, error);
+        } else {
+          return done(undefined, result);
+        }
+      });
+    }
+  })
+}
+
+module.exports = {
+  verifyToken,
+};
