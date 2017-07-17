fb7e3a7ca9778708fbe15107c08e89e34ca749b1
JananiVenkat <jananivenkat94@gmail.com>
2017-07-17 13:43:24 +0530

    added router to the eventmapping

--- server/api/v1/modules/eventmapping/eventmapping.router.js
+++ server/api/v1/modules/eventmapping/eventmapping.router.js
@@ -1,1 +1,16 @@
+const router = require('express').Router();
 
+const eventmappingCtrl = require('./eventmapping.controller');
+
+
+router.get('/events', (req, res) => {
+	eventmappingCtrl.getmappingDetails(tooldata, (err, results) => {
+		if(err) {
+			return res.send(err);
+		} else {
+		return res.send (results);
+	}
+	});
+});
+
+module.exports = router;
