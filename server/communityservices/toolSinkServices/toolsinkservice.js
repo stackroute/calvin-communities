396853372355240eb756d591ec0acbee69abddd0
sandhyasrini <sandy150225@gmail.com>
2017-07-18 16:18:08 +0530

    added a service to listen from tool events and publish to Community activity events

--- server/communityservices/toolSinkServices/toolsinkservice.js
+++ server/communityservices/toolSinkServices/toolsinkservice.js
@@ -1,22 +1,16 @@
 const toolSinkService = require('../../api/v1/modules/communitytools/communitytools.controller');
+const registerPublisherService = require('../../common/kafkaPublisher');
+const logger = require('../../logger.js');
 module.exports = function(eventMessage) {
-  console.log('toolsink consumed the event: ', eventMessage);
-  // console.log(eventMessage.type);
-    toolSinkService.checkTool(eventMessage.tools, eventMessage.domain, (err, res) => {
-      console.log('Registered tool successfully');
-      if(!err) {
-      toolSinkService.getTools(eventMessage.tools, eventMessage.domain), (error, result) => {
-      	if(!error) {
-      		console.log(result);
-      	} else {
-      		console.log(error);
-      	}
-      }
-    } else {
-    	console.log(err);
+  logger.debug('toolsink consumed the event: ', eventMessage);
+  // message = { domain: dataFromURI, tools: dataFromBody, type: 'addtool' };
+  let message = JSON.stringify(eventMessage);
+  logger.debug("sending message", message);
+  registerPublisherService.publishToTopic('CommunityActvityEvents', message, (err, res) => {
+    if (err) {
+      logger.debug('error occured', err);
+    } else if (res) {
+      logger.debug('result is', res);
     }
-    });
-
+  });
 };
-
-
