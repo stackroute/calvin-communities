30717fed7e6761722f881faf8eb627d0f778f052
psharneja <psharneja@gmail.com>
2017-07-18 19:51:23 +0530

    patch requestof tools, added events in jwt token

--- server/api/v1/modules/communitytoolmapping/communitytoolmapping.controller.js
+++ server/api/v1/modules/communitytoolmapping/communitytoolmapping.controller.js
@@ -9,8 +9,8 @@ const token = require('../../../../config').jwtdetails;
 
 const COMMUNITY_TOOL_EVENT_MAP = 'communitytooleventmap';
 
-function authenticate(domain, toolid, done) {
-  jwt.sign({ domain: domain, toolid: toolid }, token.secret, (err, code) => {
+function authenticate(domain, toolid, eventids, done) {
+  jwt.sign({ domain: domain, toolid: toolid, events: eventids }, token.secret, (err, code) => {
     if (err) { logger.debug(err); return done([400, 'Error in Operation']) }
     if (code) return done(undefined, code);
   })
@@ -32,6 +32,7 @@ function postEventMapping(parameters, details, done) {
   let wrongvalues = 0;
   const queries = [];
   let query;
+  const eventids = [];
   if (!_.has(parameters, 'domain') || !_.has(parameters, 'toolid')) return done([400, 'Domain and Toolid Required']);
   details.events.forEach((data) => {
     if (!_.has(data, 'eventid') || !_.has(data, 'eventname') || !_.has(data, 'description') || !_.has(data, 'activity') ||
@@ -39,6 +40,7 @@ function postEventMapping(parameters, details, done) {
       wrongvalues++;
     }
     query = 'insert into ' + COMMUNITY_TOOL_EVENT_MAP + '(domain, toolid, eventid, eventname, description, activity,actor, object, metadata) values (?,?,?,?,?,?,?,?,?)';
+    eventids.push(data.eventid);
 
     queries.push({
       query,
@@ -48,12 +50,11 @@ function postEventMapping(parameters, details, done) {
     });
   });
 
-  console.log(wrongvalues, "here");
   if (wrongvalues === 0) {
     async.waterfall([
       eventmappingServices.getToolMapping.bind(null, parameters),
       eventmappingServices.postEventMapping.bind(null, queries),
-      authenticate.bind(null, parameters.domain, parameters.toolid, done)
+      authenticate.bind(null, parameters.domain, parameters.toolid, eventids, done)
     ], (err, result) => {
       if (err) { logger.error('err', err); return done([400, 'Seems you\'re trying to reintegrate this tool with same domain']); }
       if (result) done(undefined, result);
@@ -66,26 +67,30 @@ function postEventMapping(parameters, details, done) {
 function updateEventMapping(parameters, details, done) {
   let wrongvalues = 0;
   const queries = [];
+  const eventids = [];
   let query;
-  details.forEach((data) => {
+
+  details.events.forEach((data) => {
     if (!_.has(data, 'eventname') || !_.has(data, 'description') || !_.has(data, 'eventid') ||
       !_.has(data, 'activity') || !_.has(data, 'actor') || !_.has(data, 'object') || !_.has(data, 'metadata')) {
       wrongvalues++;
     }
-    query = `update ${COMMUNITY_TOOL_EVENT_MAP} set eventname=?, description=?, activity=? , actor =?, object=?metadata=? where domain=? and toolid=? and eventid=?`;
+    console.log('hii')
+    eventids.push(data.eventid);
+    query = `update ${COMMUNITY_TOOL_EVENT_MAP} set eventname=?, description=?, activity=? , actor =?, object=?, metadata=? where domain=? and toolid=? and eventid=?`;
 
     queries.push({ query, params: [data.eventname, data.description, data.activity, data.actor, data.object, data.metadata, parameters.domain, parameters.toolid, data.eventid] });
   });
-  console.log('out')
+  console.log(eventids)
   if (wrongvalues === 0) {
     async.waterfall([
       eventmappingServices.getToolMapping.bind(null, parameters),
       eventmappingServices.updateEventMapping.bind(null, queries),
-      authenticate.bind(null, parameters.domain, parameters.toolid, done)
+      authenticate.bind(null, parameters.domain, parameters.toolid, eventids, done)
 
     ], (err, result) => {
       if (err) { logger.error('err', err); return done([400, 'Unexpected Error, or maybe the tool isn\'t integrated yet']); }
-      if (result) return done(undefined, result);
+      if (result) console.log(result); return done(undefined, result);
     });
   } else {
     done([400, 'Required data inputs were not found']);
