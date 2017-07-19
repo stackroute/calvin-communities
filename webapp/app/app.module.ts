af38f454b9ca9f84f45da47ad48ade4ad4ec196c
SandhiyaViswanathan <sandhiya3795@gmail.com>
2017-07-19 08:06:17 +0530

    client socket complted

--- webapp/app/app.module.ts
+++ webapp/app/app.module.ts
@@ -36,6 +36,7 @@ import { CommunityPageComponent } from './community-page/community-page.componen
 import { FootNoteComponent } from './foot-note/foot-note.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { MdListModule } from '@angular/material';
+
 @NgModule({
   declarations: [
   AppComponent,
