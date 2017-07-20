cfffdac4e6f051706bccabbd92a21caf939ea3f8
psharneja <psharneja@gmail.com>
2017-07-20 18:23:20 +0530

    changes in docker for kafka

--- server/notifications.service.js
+++ server/notifications.service.js
@@ -2,5 +2,5 @@ const registerConsumerService = require('./common/kafkaConsumerService');
 const communityNotificationService = require('./communityservices/communityNotificationService');
 
 
-registerConsumerService(['CommunityLifecycleEvents','CommunityActivityEvents' ], { autoCommit: true }, communityNotificationService);
+registerConsumerService(['CommunityLifecycleEvents'], { autoCommit: true }, communityNotificationService);
 
